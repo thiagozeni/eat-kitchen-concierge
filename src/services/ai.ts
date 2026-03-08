072e4a3d1c670fb023ff26f867ccbf77693e5914
Zeni <thiagoazeni@gmail.com>
2026-03-08 20:11:47 -0300

    Atualização de imagens locais

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -51,32 +51,32 @@ D) SE O CLIENTE PEDIR PARA VER "TODOS" OS PRATOS, ENTRADAS OU SOBREMESAS:
 Sempre que recomendar um prato, você DEVE exibir a foto dele diretamente no chat usando Markdown: ![Nome do Prato](URL).
 
 Utilize EXCLUSIVAMENTE estas URLs oficiais para os pratos correspondentes:
-- AMALFI: https://www.dropbox.com/scl/fi/btnxm36usm0lzxcsj6oiv/Amalfi.png?rlkey=7c0s4m4i1t5tzkrqwvu6rk38i&st=fl4zdoim&raw=1
-- BROWNIE POOL: https://www.dropbox.com/scl/fi/68nxsz313oknljvnv2fi1/Brownie-Pool.png?rlkey=vry8nf59mo21xf9wvmb19c3ln&st=21gim7n8&raw=1
-- CEVEAT: https://www.dropbox.com/scl/fi/4onomxas2u8g3e21os3ca/Ceveat.png?rlkey=vpnqb1rsbvg37zvp7g1uvgigc&st=aug7k7xk&raw=1
-- CHICKEN CURRY: https://www.dropbox.com/scl/fi/sct5dwsbqvk2mttsqnlh7/Chicken-Curry.png?rlkey=cm1kj59z3hqa2rk45pfxqwd2i&st=0vih21mg&raw=1
-- CIAO: https://www.dropbox.com/scl/fi/fexethu20do7u2vpv9n3x/Ciao.png?rlkey=dkbgrtznzsr67qr7ll5e16yq7&st=o7g2k8ab&raw=1
-- CRISPY CHICKEN FINGERS: https://www.dropbox.com/scl/fi/2ltp60wpzlz0msz1qigmb/Crispy-Chicken-Fingers.png?rlkey=nhvwneai51fyuyych1fqvs6l4&st=eos4yaw3&raw=1
-- EAT NHOQUE: https://www.dropbox.com/scl/fi/9dcqrd6xp4lenbjiv7qaw/Eat-Nhoque.png?rlkey=rbld0sipn0k73s4j3i00bu6k5&st=ko7cc8e7&raw=1
-- EAT CAESAR SALAD: https://www.dropbox.com/scl/fi/zzihdxszk0bur5w4zhiyf/Eat-Caesar-Salad.png?rlkey=rfet3v7d50ktjonm9ey8xqtma&st=zdab63x9&raw=1
-- FEITO: https://www.dropbox.com/scl/fi/dqgfhu303rkk5drbkys8h/Feito.png?rlkey=0x5bdl1l6fh181g34qydn94zo&st=jeocu916&raw=1
-- FRIED RICE: https://www.dropbox.com/scl/fi/xma73jpxlg71hcvvo0zuh/Fried-Rice.png?rlkey=ueuscp5f6tvz5362jjr9jtp5e&st=cnbp7xoz&raw=1
-- HONEY FIG BURRATA: https://www.dropbox.com/scl/fi/gui701t82jgl8wbreabxc/Honey-Fig-Burrata.png?rlkey=8xto7x3d0xlfnweye06s3v776&st=w5nwslk7&raw=1
-- KIDS PASTA: https://www.dropbox.com/scl/fi/wu202zq3qecx0fgzoayn0/Kids-Pasta.png?rlkey=a2q5m0k6qcyjka2m8g7gsj5z6&st=39n2p00f&raw=1
-- MAR: https://www.dropbox.com/scl/fi/0hx9navhfwv4mnt0o8tgk/Mar.png?rlkey=6w3n9pzdq9zdx5d1kfqa86ozj&st=s805cs4m&raw=1
-- MEAT: https://www.dropbox.com/scl/fi/g0s0p6ytzi7lbsq5heurb/Meat.png?rlkey=szse12mbo9vnvua6comwau1pr&st=x4urrxet&raw=1
-- NOFF: https://www.dropbox.com/scl/fi/uyenvzhc9vnte5gers2if/Noff.png?rlkey=ijg83vpjgedezzj6j67xd86jj&st=2ve24ej4&raw=1
-- PANNACOTA DE MATCHA: https://www.dropbox.com/scl/fi/we40zwmzm2v9iiylff6gn/Pannacota-de-Matcha-Proteica.png?rlkey=0l5h9rvv60aj4lo4khe4xv8b5&st=pnl3ic7g&raw=1
-- PELEIA: https://www.dropbox.com/scl/fi/re4ewncx6gjqm4n0cv1yf/Peleia.png?rlkey=eiuf2e55q2hhic54sp4wu1agn&st=uoyqahv4&raw=1
-- POK(EAT): https://www.dropbox.com/scl/fi/vpjssfdu3ya66604aenx8/Pok-eat.png?rlkey=04vza2fj9am7aw0rtn0ivro55&st=z7gs12k5&raw=1
-- RISOTO NEGRO: https://www.dropbox.com/scl/fi/1hiy2rmkb54qfw2jyqspp/Risoto-Negro.png?rlkey=h431j26ff8grt3rnk0644tuel&st=rjyxo17g&raw=1
-- RISOTO PUMPKIN: https://www.dropbox.com/scl/fi/dy1jevx0ff31ijlp9jh8t/Risoto-Pumpkin.png?rlkey=dj8eg4twsfshn2vziws0fba1d&st=7t7m5om8&raw=1
-- SALTED CARAMEL BLONDIE: https://www.dropbox.com/scl/fi/gkmxhfgl0m31vlg55rxir/Salted-Caramel-Blondie.png?rlkey=7gi4elqfvpk0zoo046pee3xpu&st=4i32slf2&raw=1
-- SWEET POTATO FRIES: https://www.dropbox.com/scl/fi/0d094a4p3es7tsygfwvmn/Sweet-Potato-Fries.png?rlkey=g9losbdigw505iqjqpyncz458&st=1zvk2zwn&raw=1
-- TAPIOCA BITES: https://www.dropbox.com/scl/fi/14o66c2ckfvvamhbjh46m/Tapioca-Bites.png?rlkey=flcuix7in082wqfw1o0d3m5i8&st=7d8ku7ss&raw=1
-- THAI PASTA: https://www.dropbox.com/scl/fi/aj0mvnh2hjghjrcoyttdt/Thai-Pasta.png?rlkey=qev2evrty5wfdqi50ac67m85x&st=v5dbwl0m&raw=1
-- TROPICAL: https://www.dropbox.com/scl/fi/tx8ihyxnl9o1hn496rpv7/Tropical.png?rlkey=mti47jivbk0wqty073xou2rtu&st=mjq1avjw&raw=1
-- VEGGIE: https://www.dropbox.com/scl/fi/hjwyr4pzvtd6ukqdp208w/Veggie.png?rlkey=0mpkacq1nd72konipbcvkjj6p&st=4qlsyxmb&raw=1
+- AMALFI: /images/dishes/Amalfi.png
+- BROWNIE POOL: /images/dishes/Brownie-Pool.png
+- CEVEAT: /images/dishes/Ceveat.png
+- CHICKEN CURRY: /images/dishes/Chicken-Curry.png
+- CIAO: /images/dishes/Ciao.png
+- CRISPY CHICKEN FINGERS: /images/dishes/Crispy-Chicken-Fingers.png
+- EAT NHOQUE: /images/dishes/Eat-Nhoque.png
+- EAT CAESAR SALAD: /images/dishes/Eat-Caesar-Salad.png
+- FEITO: /images/dishes/Feito.png
+- FRIED RICE: /images/dishes/Fried-Rice.png
+- HONEY FIG BURRATA: /images/dishes/Honey-Fig-Burrata.png
+- KIDS PASTA: /images/dishes/Kids-Pasta.png
+- MAR: /images/dishes/Mar.png
+- MEAT: /images/dishes/Meat.png
+- NOFF: /images/dishes/Noff.png
+- PANNACOTA DE MATCHA: /images/dishes/Pannacota-de-Matcha-Proteica.png
+- PELEIA: /images/dishes/Peleia.png
+- POK(EAT): /images/dishes/Pok-eat.png
+- RISOTO NEGRO: /images/dishes/Risoto-Negro.png
+- RISOTO PUMPKIN: /images/dishes/Risoto-Pumpkin.png
+- SALTED CARAMEL BLONDIE: /images/dishes/Salted-Caramel-Blondie.png
+- SWEET POTATO FRIES: /images/dishes/Sweet-Potato-Fries.png
+- TAPIOCA BITES: /images/dishes/Tapioca-Bites.png
+- THAI PASTA: /images/dishes/Thai-Pasta.png
+- TROPICAL: /images/dishes/Tropical.png
+- VEGGIE: /images/dishes/Veggie.png
 
 3. NUNCA apenas mande um link para o cliente clicar; a foto deve aparecer aberta na conversa.
 4. Se por algum motivo a imagem não carregar, o sistema mostrará um aviso amigável.
